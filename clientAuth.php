bb25740e5a136c4a86c8af8ecfbf5c9c90fc410a
Jongwan <jk293@njit.edu>
2016-03-20 23:37:13 -0400

    Auth

--- clientAuth.php
+++ clientAuth.php
@@ -1,82 +1,30 @@
 #!/usr/bin/php
-<?php>
-
-
-class clientDB
+<?php
+require_once("clientDB.php.inc");
+$command = $argv[1];
+switch($command)
 {
-    private $db;
-    public function __construct()
-    $this->db = new mysqli("local host", "root", "password", "IT202");
-
-    if ($this->db-> connect_errno > 0)
-    {
-	echo __FILE__.__LINE__.:"failed to connect to database re: ".$this->db->connect_error.PHP_EOL;
-	exit(0);
-    }
-    echo " db connected" __destruct()
-    public function __destruct()
-    {
-	$this->db->close();
-	echo "db closed".PHP_EOL;
-    }
-    
-    public function getClientId($name)
-    {
-	$query = "select clientId from clients where clientName = '$name';";
-	$results = $this->db->query($query);
-	if (! $results)
+    case 'register':
+	$name = $argv[2];
+	$password = $argv[3];
+	$db = new clientDB("connect.ini");
+	$db->addNewClient($name,$password);
+	break;
+    case 'login':
+	$name = $argv[2];
+	$password = $argv[3];
+	$db = new clientDB("connect.ini");
+	if ($db->validateClient($name,$password) == 0)
 	{
-	    echo "error with results: ".$this->db->error.PHP_EOL;
-	    return 0;
+		echo "invalid login!".PHP_EOL;
 	}
-	$client = $results->fetch_assoc();
-	if (isset($client['clientId']))
+	else
 	{
-	    return $client['clientId'];
+		echo "login successful!!".PHP_EOL;
 	}
-	return 0;
-    }
-    
-    public function validateClient($name,$password)
-    {
-	$query = "select * from clients where name='$name';");
-	$results + $this->db->query($query);
-	if (!results)
-	{
-	    echo "error with reults: ".$this->db->error.PHP_EOL;
-	    return false;
-	}
-	while ($clients = $results->fetch_assoc());
-	{
-	    print_r($client);
-	    echo PHP_EOL;
-	}
-    }
-    
-    public function addNewClient($name,$password)
-    {
-	$now = date("Y-m-d h:i:s",time());
-	$addQuery = "insert into clients (cleintName, clientPW, forestLogin, lastLogin) values ('$name','$password','$now','$now')";
-	echo "executing statement: \n$addQuery".PHP_EOL;
-	$results = $this->db->query($addQuery);
-	if (! $results)
-	{
-	echo "error: " 
-	}
-    }
+	break;
+    default:
+	echo "usage:\n".$argv[0]." [register|login] <username> <password>".PHP_EOL;
+	break;
 }
-
-// at this point db is connected
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
