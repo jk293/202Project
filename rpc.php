cc70027211648a2cea1eb1e3526dc405290294bb
Jongwan <jk293@njit.edu>
2016-04-06 17:47:39 -0400

    project stuffs

--- rpc.php
+++ rpc.php
@@ -1,22 +1,44 @@
-#!/usr/bin/php
 <?php
 require_once("clientDB.php.inc");
-$request = isset($_POST['request']) ? $_POST['request']:
-$response = "FUCK<p>";
+$request = $_POST['request'];
+$response = "OMG..........T^T<p>";
+
 switch($request)
 {
-    case "login":
+    case "Register":
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $login = new clientDB("connect.ini");
+        $response = $login->addNewClient($username,$password);
+        if ($response['success']===true)
+        {
+		$response = "Registration Successful!!!<p>";
+		HEADER("LOCATION: index.html");
+        }
+        else
+        {
+		$response = "Registration Failed:".$response['message']."<p>";
+		HEADER("LOCATION: register.html");
+        }
+        break;
+    case "Login":
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+	
 	$login = new clientDB("connect.ini");
+	
 	$response = $login->validateClient($username,$password);
+	
 	if ($response['success']===true)
 	{
 		$response = "Login Successful!<p>";
+		HEADER("LOCATION: neverendingEssays.html");
+		
 	}
 	else
 	{
 		$response = "Login Failed:".$response['message']."<p>";
+		HEADER ("LOCATION: index.html");
 	}
 	break;
 }
